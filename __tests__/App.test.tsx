3158ab0f7223eae3868f66aa5f8656ce6602c805
Siva Gunasehkaran <siva.gunasehkaran@hammertech.com>
2024-04-22 00:09:56 +1000

    add test App screen

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,15 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
+import renderer from 'react-test-renderer';
 import App from '../src/App';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
+describe('App', () => {
+  it('has 1 child', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree.children.length).toBe(1);
+  });
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+  it('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
