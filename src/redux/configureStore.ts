ea5ff04161d6dfd7500f6ac9b3979c299533d967
Siva Gunasehkaran <siva.gunasehkaran@hammertech.com>
2024-04-21 22:02:16 +1000

    clean up

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,5 +1,5 @@
 import {legacy_createStore as createStore, applyMiddleware} from 'redux';
-import app from '../reducers';
+import app from './reducers';
 
 import createSagaMiddleware from 'redux-saga';
 import dataSaga from './saga';
--- src/redux/reducers/dataReducer.ts
+++ src/redux/reducers/dataReducer.ts
@@ -2,7 +2,7 @@ import {
   FETCHING_DATA,
   FETCHING_DATA_SUCCESS,
   FETCHING_DATA_FAILURE,
-} from '../utils/constants';
+} from '../../utils/constants';
 
 const initialState = {
   data: [],
